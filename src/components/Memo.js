c836ed1018de2fb0edce8ebe8389ac4944141732
Fumiya-Kai <bunjyo.science@icloud.com>
2023-06-03 17:20:28 +0900

    Memoコンポーネントのexportと修正

--- src/components/Memo.js
+++ src/components/Memo.js
@@ -4,7 +4,7 @@ import Item from './Item';
 
 function Memo(props) {
   const [memo, setMemo] = usePersist("memo", []);
-  const [fmemo, setFmemo] = usePersist("memo", []);
+  const [fmemo, setFMemo] = usePersist("findMemo", []);
   const [mode, setMode] = usePersist('mode', 'default');
 
   let data = [];
@@ -14,7 +14,6 @@ function Memo(props) {
       data = memo.map((value, key) => (
         <Item key={value.message} value={value} index={key + 1} />
       ));
-      setMode('default');
       break;
 
     case 'find':
@@ -27,11 +26,13 @@ function Memo(props) {
       data = memo.map((value, key) => (
         <Item key={value.message} value={value} index={key + 1} />
       ));
+  };
 
-    return (
-      <table className="table mt-4">
-        <tbody>{data}</tbody>
-      </table>
-    );
-  }
+  return (
+    <table className="table mt-4">
+      <tbody>{data}</tbody>
+    </table>
+  );
 }
+
+export default Memo;
